063cc993b359dea653c38fb7632589eff37632c7
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2017-12-08 03:04:22 +0100

    Alex : Adding NewsActivity (will be made prettier in next commit)

--- app/src/main/java/com/polytech/nuitdelinfo/iiioexception/Accueil.java
+++ app/src/main/java/com/polytech/nuitdelinfo/iiioexception/Accueil.java
@@ -1,73 +1,78 @@
 package com.polytech.nuitdelinfo.iiioexception;
 
-import android.app.FragmentManager;
 import android.appwidget.AppWidgetManager;
 import android.content.Intent;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
 /**
- * Created by Howard Alexandre and Tua Mathieu and Munier Rémy on 16/03/2017.
+ Created by Howard Alexandre and Tua Mathieu and Munier Rémy on 16/03/2017.
  */
 
-public class Accueil extends AppCompatActivity {
-
+public class Accueil extends AppCompatActivity
+{
+    
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate (Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
-
-        Button cmpt1 =(Button)findViewById(R.id.competence1);
-        cmpt1.setOnClickListener(new View.OnClickListener() {
+        
+        Button cmpt1 = (Button) findViewById(R.id.competence1);
+        cmpt1.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Accueil.this,AccueilSeries.class);
-                intent.putExtra("competence","competence1");
+            public void onClick (View v)
+            {
+                Intent intent = new Intent(Accueil.this, AccueilSeries.class);
+                intent.putExtra("competence", "competence1");
                 startActivity(intent);
                 finish();
             }
         });
-
-        Button cmpt2 =(Button)findViewById(R.id.competence2);
-        cmpt2.setOnClickListener(new View.OnClickListener() {
+        
+        Button cmpt2 = (Button) findViewById(R.id.competence2);
+        cmpt2.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Accueil.this,AccueilSeries.class);
-                intent.putExtra("competence","competence2");
+            public void onClick (View v)
+            {
+                Intent intent = new Intent(Accueil.this, AccueilSeries.class);
+                intent.putExtra("competence", "competence2");
                 startActivity(intent);
                 finish();
             }
         });
-
-        ImageButton traducteur =(ImageButton)findViewById(R.id.buttonTrad);
-        traducteur.setOnClickListener(new View.OnClickListener() {
+        
+        ImageButton btnNews = (ImageButton) findViewById(R.id.btnNews);
+        btnNews.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Accueil.this,Translator.class);
+            public void onClick (View v)
+            {
+                Intent intent = new Intent(Accueil.this, NewsActivity.class);
                 startActivity(intent);
                 finish();
             }
         });
-
-        Button wiwi =(Button)findViewById(R.id.widgetCustom);
-        wiwi.setOnClickListener(new View.OnClickListener() {
+        
+        Button wiwi = (Button) findViewById(R.id.widgetCustom);
+        wiwi.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Accueil.this,WidgetConfigure.class);
-                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID,1);
+            public void onClick (View v)
+            {
+                Intent intent = new Intent(Accueil.this, WidgetConfigure.class);
+                intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, 1);
                 startActivity(intent);
-
+                finish();
             }
         });
-
-
-
+        
+        
         //Failed fragment
         /*Fragment newFrag = new BaseFragment();
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
@@ -85,9 +90,9 @@ public class Accueil extends AppCompatActivity {
                 transaction.commit();
             }
         });*/
-
-
+        
+        
     }
-
-
+    
+    
 }
